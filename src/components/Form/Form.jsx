2ce18b2effaead72f137781269c76bb323e9106f
Petri36 <petriniagustin41521@gmail.com>
2022-11-27 21:59:19 -0300

    27-11 second changes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -33,34 +33,46 @@ export default function Form(props) {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="">Username:</label>
-          <input
-            name="username"
-            type="text"
-            value={userData.name}
-            onChange={handleInputChange}
-          />
-          <p className={s.error}>{errors.username ? errors.username : null}</p>
-        </div>
+      <div style={{ padding: "140px" }}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="" className={s.fuente}>
+              Username:
+            </label>
+            <input
+              name="username"
+              type="text"
+              value={userData.name}
+              onChange={handleInputChange}
+              placeholder="Type a username..."
+              className={s.inputuser}
+            />
+            <p className={s.error}>
+              {errors.username ? errors.username : null}
+            </p>
+          </div>
 
-        <div>
-          <label htmlFor="">Password:</label>
-          <input
-            name="password"
-            type="password"
-            value={userData.password}
-            onChange={handleInputChange}
-          />
-          <p className={s.error}>{errors.password ? errors.password : null}</p>
-        </div>
+          <div>
+            <label htmlFor="" className={s.fuente}>
+              Password:
+            </label>
+            <input
+              name="password"
+              type="password"
+              value={userData.password}
+              onChange={handleInputChange}
+              placeholder="Type a password..."
+              className={s.inputpass}
+            />
+            <p className={s.error}>
+              {errors.password ? errors.password : null}
+            </p>
+          </div>
 
-        <button 
-        type="submit"
-        className={s.button}>Login</button>
-      </form>
-    </div>
+          <button type="submit" className={s.button}>
+            Login
+          </button>
+        </form>
+      </div>
   );
 }
